b5a9dc98a33c5d0a6fa66c0528604a9daa28f9a5
Pranish Ranjit <166616813+Prannish@users.noreply.github.com>
2025-04-19 21:32:45 +0545

    Add files via upload

---
+++ Lab 1/armstrong or not/armstrong or not/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace armstrong_or_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine()), sum = 0, temp = num;
+            while (temp > 0)
+            {
+                int d = temp % 10;
+                sum += d * d * d;
+                temp /= 10;
+            }
+            Console.WriteLine(num == sum ? "Armstrong Number" : "Not an Armstrong Number");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 1/calculate total bill/calculate total bill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate_total_bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter units consumed: ");
+            int units = int.Parse(Console.ReadLine());
+            double bill = 80;
+            if (units > 20)
+                bill += (Math.Min(100, units) - 20) * 7.25;
+            if (units > 100)
+                bill += (units - 100) * 9.50;
+            Console.WriteLine($"Total bill: Rs. {bill}");
+            Console.ReadKey();
+
+        }
+    }
+}
