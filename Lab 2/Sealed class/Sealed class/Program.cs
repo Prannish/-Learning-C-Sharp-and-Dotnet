c3d73614d10c4e1e7b58acdad2629f6d538950dc
Pranish Ranjit <166616813+Prannish@users.noreply.github.com>
2025-05-06 15:00:43 +0545

    Add files via upload

---
+++ Lab 2/Sealed class/Sealed class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Sealed class
+sealed class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Square(int x)
+    {
+        return x * x;
+    }
+}
+
+// This will cause a compile-time error
+// class AdvancedCalculator : Calculator { }
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+        Console.WriteLine("Addition: " + calc.Add(5, 3));
+        Console.WriteLine("Square: " + calc.Square(4));
+
+        // Uncommenting below line will give error:
+        // "cannot derive from sealed type 'Calculator'"
+        // AdvancedCalculator adv = new AdvancedCalculator();
+    }
+}
---
+++ Lab 2/object types and dynamics/object types and dynamics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Using object type
+        object obj1 = 42;            // Boxing an int
+        object obj2 = "Hello";       // Storing a string
+        object obj3 = 3.14;          // Storing a double
+
+        Console.WriteLine("Object values:");
+        Console.WriteLine("obj1: " + obj1);
+        Console.WriteLine("obj2: " + obj2);
+        Console.WriteLine("obj3: " + obj3);
+
+        // Need casting to access specific methods/properties
+        string upper = ((string)obj2).ToUpper();
+        Console.WriteLine("Uppercase of obj2: " + upper);
+
+        Console.WriteLine();
+
+        // Using dynamic type
+        dynamic dyn1 = 10;
+        dynamic dyn2 = "world";
+        dynamic dyn3 = 4.5;
+
+        Console.WriteLine("Dynamic values:");
+        Console.WriteLine("dyn1 + 5 = " + (dyn1 + 5));      // Treated as int
+        Console.WriteLine("dyn2.ToUpper() = " + dyn2.ToUpper());  // Treated as string
+        Console.WriteLine("dyn3 * 2 = " + (dyn3 * 2));      // Treated as double
+
+        // No compile-time checking for dynamic
+        // But this will throw runtime error:
+        // Console.WriteLine(dyn1.NonExistentMethod()); // Uncomment to see error
+    }
+}
